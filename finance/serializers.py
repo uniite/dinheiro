63d0e3b6652a3da5545d8e13c1905744bdedbe9d
Jon Botelho <jon@jbotelho.com>
2013-07-21 21:59:15 -0400

    More fun with angular

--- finance/serializers.py
+++ finance/serializers.py
@@ -1,9 +1,11 @@
+from decimal import Decimal
+
 from rest_framework import serializers
 from finance.models import Account, Institution, Transaction
 
 
 class AccountSerializer(serializers.ModelSerializer):
-	censored_account_number = serializers.SerializerMethodField('get_censored_account_number')
+	censored_account_number = serializers.SerializerMethodField("get_censored_account_number")
 
 	class Meta:
 		model = Account
@@ -18,6 +20,11 @@ class InstitutionSerializer(serializers.ModelSerializer):
 		fields = ("id", "fid", "org", "url", "username")
 
 class TransactionSerializer(serializers.ModelSerializer):
+	amount = serializers.SerializerMethodField("get_amount")
+
 	class Meta:
 		model = Transaction
 		fields = ("id", "amount", "date", "payee", "type")
+
+	def get_amount(self, transaction):
+		return transaction.amount.quantize(Decimal("1.00"))
