ba8f77f53cad28ac3ef8296bc9aa32fc661fb8e1
Jon Botelho <jon@jbotelho.com>
2015-01-31 03:32:29 -0500

    Autoload venv and .env

--- manage.py
+++ manage.py
@@ -2,6 +2,24 @@
 import os
 import sys
 
+# Auto-load virtual env
+if os.path.exists("venv"):
+    activate_this = "venv/bin/activate_this.py"
+    execfile(activate_this, dict(__file__=activate_this))
+
+# Auto-load environment variables
+if os.path.exists(".env"):
+    with open(".env", "r") as f:
+        print "Loading .env"
+        for line in f.readlines():
+            try:
+                k,v = [x.strip() for x in line.split("=", 2)]
+                if k != "":
+                    print "--> Setting %s" % k
+                    os.environ[k] = v
+            except ValueError:
+                pass
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dinheiro.settings")
 
